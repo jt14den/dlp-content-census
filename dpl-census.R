3fe2c02447124867cd4918496fda8344b88c2027
Tim Dennis <dennis.tim@gmail.com>
2018-08-31 14:26:15 -0700

    adding new plots with ucla colors

--- dpl-census.R
+++ dpl-census.R
@@ -4,16 +4,19 @@ library(readr)
 library(tidyr)
 
 dlp_type_year <- read_csv('dlp-content-census.csv')
+dlp_durations <- read_csv('derv-durations.csv')
+dlp_billions <- read_csv('derv-billion.csv')
 
 glimpse(dlp_type_year)
 
 # The palette with grey:
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+# added first two primary colors for ucla identity
+uclaPalette <- c("#2D68C4", "#F2A900", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-
-
+# take file counts by type & year - reshape and plot
 dlp_type_year %>% 
-  select(type, `2015`, `2016`, `2017`, `2018`) %>% 
+  select(type, `2017`, `2018`) %>% 
   gather(year, count, -type) %>% 
   ggplot(aes(x=reorder(type, count), y=count, fill=year)) +
     geom_col(position = "dodge") +
@@ -21,6 +24,28 @@ dlp_type_year %>%
     labs(y = "File Count") +
     labs(x = "Content type") +
     labs(title= "Content items by type") +
-    scale_fill_manual(values=cbPalette) + 
+    scale_fill_manual(values=uclaPalette) + 
     theme_minimal()
 
+dlp_durations %>% 
+  gather(year, duration, -DerivedStats_durations_hours) %>% 
+  mutate(days = duration/24) %>% 
+  ggplot(aes(x = DerivedStats_durations_hours, y = duration, fill=year)) +
+    geom_col(position="dodge") +
+    coord_flip() +
+    labs(y = "Duration in Hours", x = "Type", fill = "Year") +
+    labs(title = "Derived Stats: Duration in Hours by Type") +
+    scale_y_continuous(labels = comma) +
+    scale_fill_manual(values=uclaPalette) +
+    theme_minimal()
+
+dlp_billions %>% 
+  gather(year, counts_billions, -DerivedStats_numcount_billions) %>% 
+  mutate(counts_mill = counts_billions*1000) %>% 
+  ggplot(aes(x=DerivedStats_numcount_billions, y = counts_billions, fill=year)) +
+    geom_col(position = "dodge") +
+    coord_flip() +
+    labs(y = "Counts in Billions", x = "Type", fill = "Year") +
+    scale_fill_manual(values=uclaPalette) +
+    theme_minimal()
+    
